a0e42572c270a50d1038092faff5c65034c27ed5
pratyushashukla <pratyusha.shukla212@gmail.com>
2021-08-03 16:10:26 +0530

    created

---
+++ ListeningFromChannel/ListeningFromChannel/Program.cs
@@ -0,0 +1,54 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Concurrent;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ListeningFromChannel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var con = "Host=localhost;Username=postgres;Password=prats212;Database=school;";
+            using var conn = new Npgsql.NpgsqlConnection(con);
+            conn.Open();
+         
+          BrokerConfig();
+
+            async Task BrokerConfig()
+            {
+                conn.Notification += (o, e) =>
+                
+                {
+                    
+                    Console.WriteLine(e.Payload);
+
+                    Trip_Demo data = JsonConvert.DeserializeObject<Trip_Demo>(e.Payload);
+
+                    Console.WriteLine(data.table);
+              
+                };
+                await using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.CommandText = "LISTEN trip_demo_channel";
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = conn;
+                    cmd.ExecuteNonQuery();
+                }
+
+                while (true)
+                {
+                    // Waiting for Event
+                    conn.Wait();
+                }
+            }
+
+
+           
+           
+
+        }
+    }
+}
---
+++ ListeningFromChannel/ListeningFromChannel/TripDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListeningFromChannel
+{
+    class TripDetails
+    {
+        public int trip_id { get; set; }
+        public string trip_name { get; set; }
+    }
+}
