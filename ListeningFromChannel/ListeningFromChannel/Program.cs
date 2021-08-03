6b821e1255cfe812287647f2af1ce0d18aeca383
Pro_Dev <61268026+ProDev2000-sys@users.noreply.github.com>
2021-08-03 17:43:16 +0530

    aDDED

--- ListeningFromChannel/ListeningFromChannel/Program.cs
+++ ListeningFromChannel/ListeningFromChannel/Program.cs
@@ -4,16 +4,19 @@ using System.Collections.Concurrent;
 using Newtonsoft.Json;
 using System.Threading.Tasks;
 using System.Data;
-
 namespace ListeningFromChannel
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var con = "Host=localhost;Username=postgres;Password=prats212;Database=school;";
+            var con = "Host=localhost;Username=postgres;Password=sps@7890;Database=school;";
             using var conn = new Npgsql.NpgsqlConnection(con);
             conn.Open();
+
+            ConcurrentDictionary<int, TripDetails> tripDict = new ConcurrentDictionary<int, TripDetails>();
+            Console.WriteLine("HI");
+           
          
           BrokerConfig();
 
@@ -23,12 +26,35 @@ namespace ListeningFromChannel
                 
                 {
                     
+                    
                     Console.WriteLine(e.Payload);
 
                     Trip_Demo data = JsonConvert.DeserializeObject<Trip_Demo>(e.Payload);
 
                     Console.WriteLine(data.table);
-              
+
+                    var dictData = JsonConvert.SerializeObject(data.row);
+
+                    string operationType = data.type;
+                    TripDetails td = JsonConvert.DeserializeObject<TripDetails>(dictData);
+
+                    if (operationType.Equals("INSERT") || operationType.Equals("UPDATE"))
+                    {
+                        tripDict.AddOrUpdate(td.trip_id,td,(key,oldvalue)=>td);
+
+                    }
+
+                    else if (operationType.Equals("DELETE"))
+                    {
+                        TripDetails incident;
+                        tripDict.TryRemove(td.trip_id, out incident);
+                    }
+
+                    foreach (var i in tripDict)
+                    {
+                        Console.WriteLine("Key:{0}"+" "+"Value:{1}",i.Key,i.Value.trip_name);
+
+                    }
                 };
                 await using (var cmd = new NpgsqlCommand())
                 {
@@ -37,6 +63,8 @@ namespace ListeningFromChannel
                     cmd.Connection = conn;
                     cmd.ExecuteNonQuery();
                 }
+
+                
 
                 while (true)
                 {
